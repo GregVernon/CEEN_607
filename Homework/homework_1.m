bb0661599901b0266ba8d9316c79c214b2c636a7
Greg Vernon <greg.j.vernon@gmail.com>
2020-01-14 14:35:37 -0700

    2D Mesh generation

--- Homework/homework_1.m
+++ Homework/homework_1.m
@@ -16,6 +16,28 @@ plot(x,zeros(size(x)),'-o')
 disp(eCONN)
 
 %% Functions
+%% 2D
+clear
+xMin = 0;
+xMax = 1;
+yMin = 0;
+yMax = 1;
+
+nElem{1} = 4;
+eDegree{1} = [1 2 3 4];
+eDomains{1} = linspace(xMin,xMax,nElem{1}+1);
+nElemNodes{1} = eDegree{1}+1;
+
+nElem{2} = 4;
+eDegree{2} = [4 3 2 1];
+eDomains{2} = linspace(yMin,yMax,nElem{2}+1);
+nElemNodes{2} = eDegree{2}+1;
+
+% Mesh the 2D nodes
+[x,y] = meshNodes_2D(eDomains,nElemNodes);
+
+% Build element connectivity
+eCONN = buildConnectivity_2D(nElemNodes);
 function x = meshNodes_1D(eDomains,nElemNodes)
 nElem = length(nElemNodes);
 nNodes = (nElem+1) + sum(nElemNodes-2);
@@ -31,6 +53,21 @@ for e = 1:nElem
 end
 end
 
+function [XX,YY] = meshNodes_2D(eDomains,nElemNodes)
+x = meshNodes_1D(eDomains{1},nElemNodes{1});
+y = meshNodes_1D(eDomains{2},nElemNodes{2});
+
+XX = zeros(length(x)*length(y),1);
+YY = zeros(length(x)*length(y),1);
+for jj = 1:length(y)
+    for ii = 1:length(x)
+        nodeID = (jj-1)*length(x) + ii;
+        XX(nodeID) = x(ii);
+        YY(nodeID) = y(jj);
+    end
+end
+end
+%% Element Connecitivty Functions
 function eCONN = buildConnectivity_1D(nElemNodes)
 nElem = length(nElemNodes);
 eCONN = nan(max(nElemNodes),nElem);
@@ -41,5 +78,35 @@ for e = 1:nElem
         nodeIDs = nodeIDs(end):(nodeIDs(end)+(nElemNodes(e)-1));
     end
     eCONN(1:nElemNodes(e),e) = nodeIDs;
+end
+end
+
+function eCONN = buildConnectivity_2D(nElemNodes)
+nElem = cellfun(@length,nElemNodes);
+nTotElem = prod(nElem);
+maxElemNodes = max(max(kron(nElemNodes{1},nElemNodes{2}')));
+eCONN = nan(maxElemNodes,nTotElem);
+
+nxNodes = (nElem(1)+1) + sum(nElemNodes{1}-2);
+nyNodes = (nElem(2)+1) + sum(nElemNodes{2}-2);
+
+NX = nElem(1);
+NY = nElem(2);
+for ey = 1:NY
+    for ex = 1:NX
+        e = (ey-1)*NY + ex;
+        if e == 1
+            xIndex = (1 : 1 + (nElemNodes{1}(ex)-1));
+            yIndex = (1 : 1 + (nElemNodes{2}(ey)-1));
+        elseif ex == 1
+            xIndex = (1 : 1 + (nElemNodes{1}(ex)-1));
+            yIndex = ((ey-1)+1) + sum(nElemNodes{2}(1:ey-1)-2);
+            yIndex = (yIndex(end) : (yIndex(end) + (nElemNodes{2}(ey)-1)));
+        else
+            xIndex = (xIndex(end) : (xIndex(end) + (nElemNodes{1}(ex)-1)));
+        end
+        nodeIDs = (yIndex-1)*nxNodes + xIndex';
+        eCONN(1:numel(nodeIDs),e) = nodeIDs(:);
+    end
 end
 end
