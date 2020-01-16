85ed1c832b2bbec7c2a651dfff0ca45c48d0deb3
Greg Vernon <greg.j.vernon@gmail.com>
2020-01-15 21:56:56 -0700

    Extract functions from HW1 worksheet

---
+++ Homework/buildConnectivity.m
@@ -0,0 +1,87 @@
+function eCONN = buildConnectivity(nElemNodes)
+if iscell(nElemNodes) == true
+    nDimensions = length(nElemNodes);
+    nElem = cellfun(@length,nElemNodes);
+else
+    nDimensions = 1;
+    nElem = length(nElemNodes);
+end
+
+if nDimensions == 1
+    eCONN = nan(max(nElemNodes),nElem);
+    for e = 1:nElem
+        if e == 1
+            nodeIDs = 1:nElemNodes(e);
+        else
+            nodeIDs = nodeIDs(end):(nodeIDs(end)+(nElemNodes(e)-1));
+        end
+        eCONN(1:nElemNodes(e),e) = nodeIDs;
+    end
+elseif nDimensions == 2
+    nTotElem = prod(nElem);
+    maxElemNodes = max(max(kron(nElemNodes{1},nElemNodes{2}')));
+    eCONN = nan(maxElemNodes,nTotElem);
+    
+    nxNodes = (nElem(1)+1) + sum(nElemNodes{1}-2);
+    nyNodes = (nElem(2)+1) + sum(nElemNodes{2}-2);
+    
+    NX = nElem(1);
+    NY = nElem(2);
+    for ey = 1:NY
+        for ex = 1:NX
+            e = (ey-1)*NY + ex;
+            if e == 1
+                xIndex = (1 : 1 + (nElemNodes{1}(ex)-1));
+                yIndex = (1 : 1 + (nElemNodes{2}(ey)-1));
+            elseif ex == 1
+                xIndex = (1 : 1 + (nElemNodes{1}(ex)-1));
+                yIndex = ((ey-1)+1) + sum(nElemNodes{2}(1:ey-1)-2);
+                yIndex = (yIndex(end) : (yIndex(end) + (nElemNodes{2}(ey)-1)));
+            else
+                xIndex = (xIndex(end) : (xIndex(end) + (nElemNodes{1}(ex)-1)));
+            end
+            nodeIDs = (yIndex-1)*nxNodes + xIndex';
+            eCONN(1:numel(nodeIDs),e) = nodeIDs(:);
+        end
+    end
+elseif nDimensions == 3
+    nTotElem = prod(nElem);
+    maxElemNodes = kron(kron(nElemNodes{1},nElemNodes{2}'),nElemNodes{3});
+    maxElemNodes = max(maxElemNodes(:));
+    eCONN = nan(maxElemNodes,nTotElem);
+    
+    nxNodes = (nElem(1)+1) + sum(nElemNodes{1}-2);
+    nyNodes = (nElem(2)+1) + sum(nElemNodes{2}-2);
+    nzNodes = (nElem(3)+1) + sum(nElemNodes{3}-2);
+    
+    NX = nElem(1);
+    NY = nElem(2);
+    NZ = nElem(3);
+    for ez = 1:NZ
+        for ey = 1:NY
+            for ex = 1:NX
+                e = (ey-1)*NY + ex;
+                if ex == 1 && ey == 1 && ez == 1
+                    xIndex = (1 : 1 + (nElemNodes{1}(ex)-1));
+                    yIndex = (1 : 1 + (nElemNodes{2}(ey)-1));
+                    zIndex = (1 : 1 + (nElemNodes{3}(ez)-1));
+                elseif ex == 1 && ey == 1
+                    xIndex = (1 : 1 + (nElemNodes{1}(ex)-1));
+                    yIndex = (1 : 1 + (nElemNodes{2}(ey)-1));
+                    zIndex = ((ez-1)+1) + sum(nElemNodes{1}(1:ex-1)-2) + sum(nElemNodes{2}(1:ey-1)-2);
+                    zIndex = (zIndex(end) : (zIndex(end) + (nElemNodes{3}(ez)-1)));
+                elseif ex == 1
+                    xIndex = (1 : 1 + (nElemNodes{1}(ex)-1));
+                    yIndex = ((ey-1)+1) + sum(nElemNodes{2}(1:ey-1)-2);
+                    yIndex = (yIndex(end) : (yIndex(end) + (nElemNodes{2}(ey)-1)));
+                else
+                    xIndex = (xIndex(end) : (xIndex(end) + (nElemNodes{1}(ex)-1)));
+                end
+                zIndex = reshape(zIndex,[1,1,length(zIndex)]);
+                nodeIDs = (zIndex-1)*nxNodes*nyNodes + (yIndex-1)*nxNodes + xIndex';
+                eCONN(1:numel(nodeIDs),e) = nodeIDs(:);
+            end
+        end
+    end
+end
+end
---
+++ Homework/meshNodes.m
@@ -0,0 +1,60 @@
+function varargout = meshNodes(eDomains,nElemNodes)
+if iscell(eDomains) == true
+    nDimensions = length(eDomains);
+else
+    nDimensions = 1;
+end
+
+if nDimensions == 1
+    nElem = length(nElemNodes);
+    nNodes = (nElem+1) + sum(nElemNodes-2);
+    x = zeros(nNodes,1);
+    for e = 1:nElem
+        if e == 1
+            nodeIDs = 1:nElemNodes(e);
+        else
+            nodeIDs = nodeIDs(end):(nodeIDs(end)+(nElemNodes(e)-1));
+        end
+
+        x(nodeIDs) = linspace(eDomains(e),eDomains(e+1),nElemNodes(e));
+    end
+    varargout{1} = x;
+elseif nDimensions == 2
+    x = meshNodes(eDomains{1},nElemNodes{1});
+    y = meshNodes(eDomains{2},nElemNodes{2});
+
+    XX = zeros(length(x)*length(y),1);
+    YY = zeros(length(x)*length(y),1);
+    for jj = 1:length(y)
+        for ii = 1:length(x)
+            nodeID = (jj-1)*length(x) + ii;
+            XX(nodeID) = x(ii);
+            YY(nodeID) = y(jj);
+        end
+    end
+    varargout{1} = XX;
+    varargout{2} = YY;
+    
+elseif nDimensions == 3
+    x = meshNodes(eDomains{1},nElemNodes{1});
+    y = meshNodes(eDomains{2},nElemNodes{2});
+    z = meshNodes(eDomains{3},nElemNodes{3});
+
+    XXX = zeros(length(x)*length(y)*length(z),1);
+    YYY = zeros(length(x)*length(y)*length(z),1);
+    ZZZ = zeros(length(x)*length(y)*length(z),1);
+    for kk = 1:length(z)
+        for jj = 1:length(y)
+            for ii = 1:length(x)
+                nodeID = (kk-1)*length(x)*length(y) + (jj-1)*length(x) + ii;
+                XXX(nodeID) = x(ii);
+                YYY(nodeID) = y(jj);
+                ZZZ(nodeID) = z(kk);
+            end
+        end
+    end
+    varargout{1} = XXX;
+    varargout{2} = YYY;
+    varargout{3} = ZZZ;
+end
+end
