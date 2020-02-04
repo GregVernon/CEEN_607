cbf2c1b21a33ab383306c3aead8e31bf8fd5124e
Greg Vernon <greg.j.vernon@gmail.com>
2020-02-04 15:29:00 -0700

    Add enumerations for basis functions

--- src/feEnumerations.jl
+++ src/feEnumerations.jl
@@ -2,9 +2,12 @@ module feEnumerations
 
 export enum_BoundaryCondition
 export enum_LoadCondition
+export enum_MeshEntity
+export enum_BasisFunction
 
 @enum enum_BoundaryCondition dirichlet neumann
 @enum enum_LoadCondition pressure traction force
 @enum enum_MeshEntity element node
+@enum enum_BasisFunction bernstein lagrange legendre 
 
 end
