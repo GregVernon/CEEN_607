5caf6cf481fd8b060571ebef92c1b30f580366d4
Greg Vernon <greg.j.vernon@gmail.com>
2020-02-15 18:09:11 -0700

    Update quadrature dimensioning

--- src/feCode.jl
+++ src/feCode.jl
@@ -37,11 +37,11 @@ end
 
 function GaussQuadratureRule_1D(nPts)
     if nPts == 1
-        ξ = [0.]
-        W = [2.]
+        ξ = NamedDimsArray{(:local_qp_id,)}([0.])
+        W = NamedDimsArray{(:local_qp_id,)}([2.])
     elseif nPts == 2
-        ξ = [-1/sqrt(3), 1/sqrt(3)]
-        W = [1., 1.,]
+        ξ = NamedDimsArray{(:local_qp_id,)}([-1/sqrt(3), 1/sqrt(3)])
+        W = NamedDimsArray{(:local_qp_id,)}([1., 1.,])
     end
     return ξ,W
 end
@@ -53,7 +53,7 @@ function GaussQuadratureRule_2D(nPts)
 
     nQP = nPts^2
     QP = NamedDimsArray{(:local_qp_id, :ℜ_2)}(zeros(Float64,nQP,2))
-    QW = NamedDimsArray{(:local_qp_id, :qp_weight)}(zeros(Float64,nQP,1))
+    QW = NamedDimsArray{(:local_qp_id,)}(zeros(Float64,nQP))
     n = 0
     for j = 1:nPts
         for i = 1:nPts
@@ -73,7 +73,7 @@ function GaussQuadratureRule_3D(nPts)
 
     nQP = nPts^3
     QP = NamedDimsArray{(:local_qp_id, :ℜ_3)}(zeros(Float64,nQP,3))
-    QW = NamedDimsArray{(:local_qp_id, :qp_weight)}(zeros(Float64,nQP,1))
+    QW = NamedDimsArray{(:local_qp_id,)}(zeros(Float64,nQP))
     n = 0
     for k = 1:nPts
         for j = 1:nPts
