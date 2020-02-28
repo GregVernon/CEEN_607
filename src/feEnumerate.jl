457b0b2b07d3fe65c292d640f851c01b336fa5c3
Greg Vernon <greg.j.vernon@gmail.com>
2020-02-28 12:24:31 -0700

    Encapsulate feEnumerate as a module

--- src/feEnumerate.jl
+++ src/feEnumerate.jl
@@ -1,2 +1,8 @@
-@enum enum_BoundaryCondition dirichlet neumann
-@enum enum_LoadCondition body pressure traction force
+module feEnumerate
+export enum_BoundaryCondition
+export enum_LoadCondition
+
+@enum enum_BoundaryCondition unconstrained dirichlet neumann
+@enum enum_LoadCondition noload body pressure traction force
+
+end
