66b1fdb12a07a1041fa97b21c1a26fcef86cdaab
Greg Vernon <greg.j.vernon@gmail.com>
2020-04-01 01:01:59 -0600

    Create feElementDeformed.m

---
+++ src/feElementDeformed.m
@@ -0,0 +1,32 @@
+classdef feElementDeformed
+    %feElementConfiguration Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        % Descriptors
+        Degree
+        Dimension
+        GlobalID
+        Type
+                
+        % Children
+        Nodes
+        Quadrature
+        
+        % Physics
+        MaterialConstitutiveMatrix
+        
+        % Linear Algebra Stuff
+        StiffnessMatrix
+        InternalForceVector
+        ExternalForceVector
+    end
+    
+    methods
+        % Constructors
+        function obj = feElementDeformed()
+            %feElementConfiguration Construct an instance of this class
+        end
+    end
+end
+
