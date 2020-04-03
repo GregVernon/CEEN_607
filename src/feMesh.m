95e9373824062fa2d9c65b647dc10273be5d65a0
Greg Vernon <greg.j.vernon@gmail.com>
2020-04-03 10:09:53 -0600

    Add function to assign load conditions to the mesh

--- src/feMesh.m
+++ src/feMesh.m
@@ -5,6 +5,9 @@ classdef feMesh
     properties
         NodeConnectivity
         DOFConnectivity
+        ElementSets feElementSet
+        SurfaceSets feSurfaceSet
+        NodeSets    feNodeSet
         Elements    feElement
     end
     
@@ -86,6 +89,60 @@ classdef feMesh
                 f = obj.Elements(e).Reference.InternalForceVector;
                 local_2_global_dof = obj.Elements(e).DOFConnectivity(:);
                 Fint(local_2_global_dof) = f;
+            end
+        end
+        function obj = assignLoadConditions(obj, LC)
+            num_load_conditions = length(LC);
+            for load_id = 1:num_load_conditions
+                if     strcmpi(LC(load_id).Type, "body")
+                    ElementSet = LC(load_id).ElementSet;
+                    ElemID = ElementSet.ElementID;
+                    M = LC(load_id).Magnitude;
+                    D = LC(load_id).Direction;
+                    BF = M * (D ./ norm(D));
+                    for loc_elem_id = 1:length(ElemID)
+                        global_elem_id = ElemID(loc_elem_id);
+                        obj.Elements(global_elem_id).Reference.BodyForce = BF;
+                    end
+                elseif strcmpi(LC(load_id).Type, "pressure")
+                    SurfaceSet = LC(load_id).SurfaceSet;
+                    ElemID = SurfaceSet.ElementID;
+                    M = LC(load_id).Magnitude;
+                    for loc_elem_id = 1:length(ElemID)
+                        % Get global element id
+                        global_elem_id = ElemID(loc_elem_id);
+                        % Get local element side id
+                        loc_side_id = SurfaceSet.LocalSideID;
+                        % Assign pressure to local element side id
+                        obj.Elements(global_elem_id).Reference.SurfacePressure{loc_side_id} = M;
+                    end
+                elseif strcmpi(LC(load_id).Type, "force")
+                    if     isempty(LC(load_id).SurfaceSetName) == false
+                        % Node Force defined on all nodes in a surface set
+                        SurfaceSet = LC(load_id).SurfaceSet;
+                        ElemID = SurfaceSet.ElementID;
+                        M = LC(load_id).Magnitude;
+                        D = LC(load_id).Direction;
+                        BF = M * (D ./ norm(D));
+                        for loc_elem_id = 1:length(ElemID)
+                            global_elem_id = ElemID(loc_elem_id);
+                            loc_node_id = NodeSet.LocalNodeID(loc_elem_id);
+                            obj.Elements(global_elem_id).Reference.NodeForce{loc_node_id} = BF;
+                        end
+                    elseif isempty(LC(load_id).NodeSetName) == false
+                        % Node Force defined on all nodes in a nosde set
+                        NodeSet = LC(load_id).NodeSet;
+                        ElemID = NodeSet.ElementID;
+                        M = LC(load_id).Magnitude;
+                        D = LC(load_id).Direction;
+                        BF = M * (D ./ norm(D));
+                        for loc_elem_id = 1:length(ElemID)
+                            global_elem_id = ElemID(loc_elem_id);
+                            loc_node_id = NodeSet.LocalNodeID(loc_elem_id);
+                            obj.Elements(global_elem_id).Reference.NodeForce{loc_node_id} = BF;
+                        end
+                    end
+                end
             end
         end
     end
