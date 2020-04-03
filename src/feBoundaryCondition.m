8e8beb2fb1ec0eef6360afdfc28d1481a23bc414
Greg Vernon <greg.j.vernon@gmail.com>
2020-04-02 22:46:05 -0600

    Add classes for load and boundary conditions

---
+++ src/feBoundaryCondition.m
@@ -0,0 +1,19 @@
+classdef feBoundaryCondition
+    %feBoundaryCondition Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        Type
+        NodeSetName
+        DOF
+        Value
+    end
+    
+    methods
+        function obj = feBoundaryCondition()
+            %feBoundaryCondition Construct an instance of this class
+            %   Detailed explanation goes here
+        end
+    end
+end
+
---
+++ src/feLoadCondition.m
@@ -0,0 +1,20 @@
+classdef feLoadCondition
+    %feLoadCondition Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        Type
+        ElementSetName
+        SurfaceSetName
+        Direction
+        Magnitude
+    end
+    
+    methods
+        function obj = feLoadCondition()
+            %feLoadCondition Construct an instance of this class
+            %   Detailed explanation goes here
+        end
+    end
+end
+
