e20f78ed7bb191385507f83b93d6e39540f00ced
Greg Vernon <greg.j.vernon@gmail.com>
2020-04-04 16:03:30 -0600

    Update worksheet

--- src/worksheet.m
+++ src/worksheet.m
@@ -1,18 +1,29 @@
+%%
 clear
-M = importMesh("mesh/test_1x1.mat");
+clc
+filename = "mesh/Case_A_param.input";
 
-num_elems = length(M.Elements);
-e = 1;
+[M,P] = importInput(filename);
 
-d_coeff = {[0; 0];[0.1; 0];[0; 0];[0.1; 0]};
+SP = feSolveParameters();
+sol = feSolve(M,P,SP).solve()
 
+%%
+clear
+clc
+filename = "mesh/Case_B_param.input";
 
-M.Elements(e).Reference.MaterialConstitutiveMatrix = feElement.compute_material_consitutive_matrix(1,0);
-M.Elements(e).Reference.DirichletConditions = {[0 0]; [0.1 missing]; [0 0]; [0.1 missing]};
-M.Elements(e).Reference.BodyForce = [0; 0];
-M.Elements(e).Reference.NodeForce = {[0; 0]; [0; 0]; [0; 0]; [0; 0]};
-M.Elements(e).Reference.SurfacePressure = {[0]; [0]; [0]; [0]};
+[M,P] = importInput(filename);
 
-M.Elements(e).Reference.StiffnessMatrix = M.Elements(e).compute_local_stiffness_matrix();
-M.Elements(e).Reference.InternalForceVector = M.Elements(e).compute_local_internalforce_vector(d_coeff);
-M.Elements(e).Reference.ExternalForceVector = M.Elements(e).compute_local_externalforce_vector();
+SP = feSolveParameters();
+sol = feSolve(M,P,SP).solve()
+
+%%
+clear
+clc
+filename = "mesh/Case_C_param.input";
+
+[M,P] = importInput(filename);
+
+SP = feSolveParameters();
+sol = feSolve(M,P,SP).solve()
